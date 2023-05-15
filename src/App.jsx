52e925b557e6a3cd8dd45dbc42a462cc5ac7f8cc
Shifat ullah <shifatullah128@gmail.com>
2023-05-15 13:58:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center  text-amber-700 text-6xl'>rofrk</h1>
+      <button className="btn btn-active  ml-5 btn-secondary">Button</button>
+<button className="btn btn-active btn-accent">Button</button>
+    </div>
+  );
+};
+
+export default App;
